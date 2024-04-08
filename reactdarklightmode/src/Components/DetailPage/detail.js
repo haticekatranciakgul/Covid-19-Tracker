b09fa18ce3e536ab8b1fa7cfac0329c4c7c14ec4
Hatice KATRANCI AKGÜL <76768140+haticekatranciakgul@users.noreply.github.com>
2024-04-08 16:45:08 +0300

    css

--- reactdarklightmode/src/Components/DetailPage/detail.js
+++ reactdarklightmode/src/Components/DetailPage/detail.js
@@ -5,44 +5,96 @@ import { ColorModeContext } from "../../theme";
 import { ThemeProvider, useTheme } from "@mui/material";
 import numeral from "numeral";
 import { useParams } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function Detail() {
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-  const [countryInfo, setCountryInfo] = useState({});
-  const { country } = useParams();
+    const theme = useTheme();
+    const colorMode = useContext(ColorModeContext);
+    const [countryInfo, setCountryInfo] = useState({});
+    const { country } = useParams();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://disease.sh/v3/covid-19/countries/${country}`);
-        const data = await response.json();
-        setCountryInfo(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://disease.sh/v3/covid-19/countries/${country}`);
+                const data = await response.json();
+                setCountryInfo(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-    fetchData();
-  }, [country]);
+        fetchData();
+    }, [country]);
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          {(countryInfo && countryInfo.countryInfo) && (
-            <div className="info-container">
-              <div className="info-flag" style={{ backgroundImage: `url(${countryInfo.countryInfo.flag})` }} />
-              <div className="info-confirmed">Cases: {numeral(countryInfo.cases).format("0,0")}</div>
-              <div className="info-recovered">Recovered: {numeral(countryInfo.recovered).format("0,0")}</div>
-              <div className="info-deaths">Deaths: {numeral(countryInfo.deaths).format("0,0")}</div>
-            </div>
-          )}
-        </Container>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Container maxWidth="xl">
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+                                {/* <Item> {(countryInfo && countryInfo.countryInfo) && (
+                                    <div className="info-container">
+                                        <div className="info-flag" style={{ backgroundImage: `url(${countryInfo.countryInfo.flag})` }} />
+                                        <div className="info-confirmed">Cases: {numeral(countryInfo.cases).format("0,0")}</div>
+                                        <div className="info-recovered">Recovered: {numeral(countryInfo.recovered).format("0,0")}</div>
+                                        <div className="info-deaths">Deaths: {numeral(countryInfo.deaths).format("0,0")}</div>
+                                    </div>
+                                )}</Item> */}
+
+                                {(countryInfo && countryInfo.countryInfo) && (
+                                    <Card sx={{ maxWidth: 345 , padding: 3}}>
+                                        <CardActionArea >
+                                            <CardMedia
+                                                component="img"
+                                                height="200"
+                                                style={{ backgroundImage: `url(${countryInfo.countryInfo.flag})` }}
+                                            />
+                                            <CardContent>
+                                                <h1>{country}</h1>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                Cases: {numeral(countryInfo.cases).format("0,0")}
+                                                </Typography>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                Recovered: {numeral(countryInfo.recovered).format("0,0")}
+                                                </Typography>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                Deaths: {numeral(countryInfo.deaths).format("0,0")}
+                                                </Typography>
+                                               
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+
+
+                                )}
+
+
+                            </Grid>
+
+                        </Grid>
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default Detail;
